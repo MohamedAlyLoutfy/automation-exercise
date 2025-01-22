19ea5c31a15d673e53068d8e05eca9df7e48ed84
Mohamed Ali <mohamedloutfy97@gmail.com>
2025-01-22 16:46:56 +0100

    FIX formatting

--- tests/integration/test_diffusion2d.py
+++ tests/integration/test_diffusion2d.py
@@ -1,8 +1,10 @@
 """
 Tests for functionality checks in class SolveDiffusion2D
 """
+
 import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from diffusion2d import SolveDiffusion2D
 
 import numpy as np
